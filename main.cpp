e93d4b613540899fa0593d9169d8b1163763fd6f
yaros <yaroslavdisiak@gmail.com>
2023-01-26 16:16:13 +0200

    ex3donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
 
+
+
+
+#include <iostream>
+using namespace std;
+int num, other=1000,res;
+void Simple_num(){
+    for (int i = other; i >1 ; --i) {
+        if(num % i == 0 && num % 1 ==0){
+            res+=1;
+        }
+    }
+    if(res == 1){
+        cout << "Number is simple";
+    }
+    else{
+        cout <<"Number is not simple";
+    }
+}
+int main(){
+    cout <<"Enter number: ";
+    cin >> num;
+    Simple_num();
+    return 0;
+}
