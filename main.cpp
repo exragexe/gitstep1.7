d4f7451127b945b4c866178e86c3d181aa9dbcd2
yaros <yaroslavdisiak@gmail.com>
2023-01-26 18:44:16 +0200

    ex7hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 
+
+#include <iostream>
+using namespace std;
+
+void True_false(){
+    const int size = 5;
+    int arr[size];
+    int max,min;
+    min = arr[0];
+    for (int i = 1; i <= size; ++i) {
+        cin>> arr[i];
+        if (i == 0 ) {
+            min = max = arr[i];
+        }
+
+        if(max< arr[i]){
+            max =arr[i];
+        }
+        if(min > arr[i]){
+            min =arr[i];
+        }
+
+    }
+    cout << "Max is "<< max<< endl;
+    cout << "Min is "<< min;
+
+}
+int main(){
+    cout <<"Enter numbers: ";
+    True_false();
+    return 0;
+}
