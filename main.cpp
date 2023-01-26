b3b1a859fc9fb039d4bd0dd33dd8fac9b973192b
yaros <yaroslavdisiak@gmail.com>
2023-01-26 18:42:22 +0200

    ex5hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 
+
+#include <iostream>
+using namespace std;
+int num,num2;
+void Max(){
+    if (num>num2){
+        cout << num <<" more then "<< num2;
+    }
+    else if(num2>num){
+        cout <<num2 <<" more then "<< num;
+    }
+    else {
+        cout << num <<" = "<< num2;
+    }
+}
+int main(){
+    cout <<"Enter first number: ";
+    cin >> num;
+    cout <<"Enter second number: ";
+    cin >> num2;
+    Max();
+    return 0;
+}
