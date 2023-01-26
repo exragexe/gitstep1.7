22d76a95d8c43a4fa4e7828e4cb74b573a22cecf
yaros <yaroslavdisiak@gmail.com>
2023-01-26 18:41:26 +0200

    ex4hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int num;
+void Cube(){
+   cout << "Cube of number "<<num<<" is "<<pow(num,3);
+}
+int main(){
+    cout <<"Enter number: ";
+    cin >> num;
+    Cube();
+    return 0;
+}
