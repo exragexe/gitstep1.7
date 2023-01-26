ef5d2c1210b26a445f715eebe391c7ac60564327
yaros <yaroslavdisiak@gmail.com>
2023-01-26 15:47:09 +0200

    ex1donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 
+
+#include <iostream>
+
+using namespace std;
+int k,n;
+void Pryamokytnik(){
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= k; ++j) {
+            cout <<"*"<<" ";
+
+        }
+        cout << endl;
+
+    }
+
+
+}
+int main(){
+    cout <<"Enter height N: ";
+    cin >> n;
+    cout <<"Enter weight K: ";
+    cin >> k;
+    cout <<endl;
+    Pryamokytnik();
+    return 0;
+}
