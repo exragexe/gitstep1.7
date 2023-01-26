b2afa5dd1fc33374ad5b1e2aa1e9f1db388a2060
yaros <yaroslavdisiak@gmail.com>
2023-01-26 16:07:58 +0200

    ex2donehw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+using namespace std;
+int formula=1,num;
+void Factorial(){
+    for (int i = num; i > 1; --i) {
+
+        formula *= i;
+        cout << " * "<< i-1;
+    }
+    cout <<" = "<< formula;
+}
+int main(){
+    cout <<"Enter factorial number: ";
+    cin >> num;
+    cout <<num<<"! = "<<num;
+    Factorial();
+    return 0;
+}
