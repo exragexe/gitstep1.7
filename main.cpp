c64c347f800970ac669f3fbe987bb907742b467e
yaros <yaroslavdisiak@gmail.com>
2023-01-26 18:43:01 +0200

    ex6hw

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 
+
+#include <iostream>
+using namespace std;
+int num;
+void True_false(){
+    if (num<0){
+        cout << "False";
+    }
+    else if(num>=1){
+        cout << "True";
+    }
+    else {
+        cout << "0 is not natural number";
+    }
+}
+int main(){
+    cout <<"Enter number: ";
+    cin >> num;
+    True_false();
+    return 0;
+}
